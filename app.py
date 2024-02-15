cd30ffa7f24818406af0aef29454cb1d4c6be6c8
Elikibs <elishakibet67@gmail.com>
2024-02-15 08:23:48 +0300

    modify user model

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_migrate import Migrate
+from models import db, User
 
 app = Flask(__name__)
 app.secret_key = 'b33b151adaccb5d08c9eb0c0'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fleets.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-# app initializing with db
+# Initialize app with db, migrate
+migrate = Migrate(app, db)
 db.init_app(app)
---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__= 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"<User {self.username}"
