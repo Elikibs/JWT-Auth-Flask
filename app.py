c82f231c22d1d41814fa1c019a881b6928010c23
Elikibs <elishakibet67@gmail.com>
2024-02-15 14:49:27 +0300

    add protected endpoints

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from models import db, User
 from auth import auth_bp
+from users import user_bp
 
 app = Flask(__name__)
 app.secret_key = '4b37674a1d174146f65484bb'
@@ -17,6 +18,7 @@ db.init_app(app)
 
 # register blueprints
 app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(user_bp, url_prefix='/users')
 
 if __name__== '__main__':
     app.run(port=5555, debug=True)
