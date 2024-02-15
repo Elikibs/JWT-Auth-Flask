8ae48296881ea5fc6f4e2eea758a0c5130f47a03
Elikibs <elishakibet67@gmail.com>
2024-02-15 14:03:15 +0300

    login blueprint

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 from models import db, User
 from auth import auth_bp
 
 app = Flask(__name__)
-app.secret_key = 'b33b151adaccb5d08c9eb0c0'
+app.secret_key = '4b37674a1d174146f65484bb'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 # Initialize app with db, migrate
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 db.init_app(app)
 
 # register blueprints
