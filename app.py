734e793c950feff5aaf428ffb06ead9a782aa4b7
Elikibs <elishakibet67@gmail.com>
2024-02-15 19:02:23 +0300

    error handlers

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from models import db, User
@@ -19,6 +19,39 @@ db.init_app(app)
 # register blueprints
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(user_bp, url_prefix='/users')
+
+# jwt error handlers
+# expired token handler
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_data):
+    return jsonify(
+        {
+            "message": "Token has expired",
+            "error": "token_expired"
+        }
+    ), 401
+
+# invalid token loader
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify(
+        {
+            "message": "Signature verification failed",
+            "error": "invalid_token"
+        }
+    ), 401
+
+# unauthorized token loader (when token is not provided)
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify(
+        {
+            "message": "Request doesn't contain a valid token",
+            "error": "authorization_header"
+        }
+    ), 401
+
+
 
 if __name__== '__main__':
     app.run(port=5555, debug=True)
